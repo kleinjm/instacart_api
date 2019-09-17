145e999dc94f0786de3007f73a0a8bf7523c91f4
James Klein <kleinjm007@gmail.com>
2019-09-17 13:22:39 -0400

    Add changelog

---
+++ lib/instacart_api.rb
@@ -0,0 +1,6 @@
+require "instacart_api/version"
+
+module InstacartApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/instacart_api_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe InstacartApi do
+  it "has a version number" do
+    expect(InstacartApi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
