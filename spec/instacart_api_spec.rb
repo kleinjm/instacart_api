4b90cb280e8621a7d2a11f64e42f09e28c8da268
James Klein <kleinjm007@gmail.com>
2020-05-14 13:03:09 -0600

    add rspec binstub

--- spec/instacart_api_spec.rb
+++ spec/instacart_api_spec.rb
@@ -1,9 +1,7 @@
+# frozen_string_literal: true
+
 RSpec.describe InstacartApi do
   it "has a version number" do
     expect(InstacartApi::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
